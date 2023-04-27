f0d9f0ef1199f8f8b21ea034a3e89db0e257b3bd
Henry Chea <work@henrychea.com>
2023-04-27 17:38:44 +0700

    Added db and css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  typescript: {
+    shim: false,
+  },
+  pages: true,
+  css: [
+    '@/assets/css/global.css',
+    '@/assets/scss/global.scss'
+  ],
+  
+});
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="p-4">
+        <h1>My Simple Todo App</h1>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
